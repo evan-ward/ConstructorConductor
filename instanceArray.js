93eb089b9ac25341019d1297f1a6cd6b110a69b4
evan-ward <evan.ward1@gmail.com>
2014-07-09 19:24:49 -0600

    completed JS exercises

--- instanceArray.js
+++ instanceArray.js
@@ -13,29 +13,48 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
+var users = [];
+
 
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
-  //code here
+var tyler = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+var cahlan = new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags');
+var lenny = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
+
+users.push(tyler);
+users.push(cahlan);
+users.push(lenny);
+
+User.prototype.showInfo = function(){
+  console.log(this);
+}
+
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
-  //code here
+tyler.showInfo();
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
-  //code here
+lenny.showInfo();
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+var evan = new User('Evan', 'evan.ward1@gmail.com', 'iLoveCheese');
+
+users.push(evan);
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name. 
+debugger;
+var names = function(){
+  for(var i = 0; i < users.length; i++){
+    alert('All my users names are ' + users[i].name);
+  }
 
-  //code here
+};
--- sayName.js
+++ sayName.js
@@ -1,13 +1,22 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
-  //code here
+var Person = function(name, age){
+  this.name = name;
+  this.age = age;
+}
 
 
 //Now create three instances of Person with data you make up
 
-  //code here
+var evan = new Person('Evan', 33);
+var megan = new Person('Megan', 31);
+var hannah = new Person('Hannah', 2);
 
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
-  //code here
+Person.prototype.showName = function(){
+  console.log(this.name);
+}
+
+evan.showName();
